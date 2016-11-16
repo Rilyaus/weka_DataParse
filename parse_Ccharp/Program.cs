ae20a49a668d43f1c10f3d8fed643a3ccbc33752
Rilyaus-nLab <Rilyaus-nLab@RILYAUS-NLAB>
2016-11-16 14:08:51 +0900

    Train, Test 셋 분할

--- parse_Ccharp/Program.cs
+++ parse_Ccharp/Program.cs
@@ -11,43 +11,69 @@ namespace parse_Ccharp
 	{
 		static void Main(string[] args)
 		{
-			string dirPath = @"c:\users\rilyaus\downloads\EC_LIB_2016\DATA";
+			string dirPath = @"c:\Users\Rilyaus-nLab\Downloads\EC_LIB_2016\DATA";
 
 			DirectoryInfo di = new DirectoryInfo(dirPath);
 
 			Console.WriteLine("Current Path : " + di.FullName);
 
 			foreach(var item in di.GetFiles()) {
-				FileStream fsa = File.Create(dirPath + @"\Parse\parse_" + item.Name);
+				FileStream fTrain = File.Create(dirPath + @"\Parse\abs_train_" + item.Name);
+                FileStream fTest = File.Create(dirPath + @"\Parse\abs_test_" + item.Name);
 
 				StreamReader sr = new StreamReader(dirPath + @"\" + item.Name);
-				StreamWriter sw = new StreamWriter(fsa);
-				List<String> oParts = new List<string>();
+                StreamWriter sTrain = new StreamWriter(fTrain);
+                StreamWriter sTest = new StreamWriter(fTest);
+                List<String> oParts = new List<string>();
 
 				int count = 0;
 
+                int lineCount = File.ReadAllLines(dirPath + @"\" + item.Name).Count();
+                int trainCount = (lineCount / 3) * 2 + (lineCount % 3) + 2;
+                
 				while( sr.Peek() >= 0 ) {
 					count++;
 					var parts = sr.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-					if( count > 2 ) {
+					if( count > 2 && count < trainCount ) {
 						for( int i=0 ; i<parts.Length ; i++ ) {
 							if( i < 4 ) {
-								sw.Write("{0,3} ", parts[i]);
+								sTrain.Write("{0,3} ", parts[i]);
 							} else if( i == 4 || i == 5 ) { // 측정 위도, 경도 값 변화량 계산
-								double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i]), 7);
-								sw.Write("{0,11:F6} ", temp);
+								double temp = Math.Round(double.Parse(parts[i]), 7); // Absolute
+                                                                                     //double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i]), 7); // Relative
+                                sTrain.Write("{0,11:F6} ", temp);
 							} else if( i == 6 || i == 7 ) { // 모델 예측 위도, 경도 값 변화량 계산
-								double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i-2]), 7);
-								sw.Write("{0,11:F6} ", temp);
+                                double temp = Math.Round(double.Parse(parts[i]), 7); // Absolute
+                                                                                     //double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i]), 7); // Relative
+                                sTrain.Write("{0,11:F6} ", temp);
 							} else if( i > 7 ) {
-								sw.Write("{0,8} ", oParts[i]);
+                                sTrain.Write("{0,8} ", oParts[i]);
 							} else {
-								sw.Write(parts[i] + " ");
+                                sTrain.Write(parts[i] + " ");
 							}
 						}
-						sw.WriteLine();
-					}
+                        sTrain.WriteLine();
+					} else if( count >= trainCount) {
+                        for (int i = 0; i < parts.Length; i++) {
+                            if (i < 4) {
+                                sTest.Write("{0,3} ", parts[i]);
+                            } else if (i == 4 || i == 5) { // 측정 위도, 경도 값 변화량 계산
+                                double temp = Math.Round(double.Parse(parts[i]), 7); // Absolute
+                              //double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i]), 7); // Relative
+                                sTest.Write("{0,11:F6} ", temp);
+                            } else if (i == 6 || i == 7) { // 모델 예측 위도, 경도 값 변화량 계산
+                                double temp = Math.Round(double.Parse(parts[i]), 7); // Absolute
+                              //double temp = Math.Round(double.Parse(parts[i]) - double.Parse(oParts[i]), 7); // Relative
+                                sTest.Write("{0,11:F6} ", temp);
+                            } else if (i > 7) {
+                                sTest.Write("{0,8} ", oParts[i]);
+                            } else {
+                                sTest.Write(parts[i] + " ");
+                            }
+                        }
+                        sTest.WriteLine();
+                    }
 
 					//if( count == 10 ) break;
 
@@ -55,9 +81,11 @@ namespace parse_Ccharp
 					oParts.AddRange(parts);
 				}
 				sr.Close();
-				sw.Close();
-				fsa.Close();
-			}
+                sTrain.Close();
+                sTest.Close();
+                fTrain.Close();
+                fTest.Close();
+            }
 
 		}
 	}
